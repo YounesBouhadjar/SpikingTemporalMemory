735fd939f61133b2a8633577304b99a55f8b482b
Younes Bouhadjar <younes.bouhadjy@gmail.com>
2023-08-31 19:26:32 +0200

    parallelize performance analysis

--- experiments/sequence_learning_and_prediction/submission_cluster_simulation.py
+++ experiments/sequence_learning_and_prediction/submission_cluster_simulation.py
@@ -9,8 +9,9 @@ import parameters_space as pars
 # get commmand line arguments
 try:
     simulation_script = sys.argv[1]
+    params_script = sys.argv[2]
 except:
-    print("provide simulation script!")
+    print("provide simulation and params script!")
 
 assert os.path.isfile('../config.yaml'), "\n>>> ERROR: Create a config file containing a dictionary with your email: config['email']" \
                                          + " and a path to where you want to store the log files config['path'] \n"
@@ -45,16 +46,16 @@ for batch_id in range(int(np.ceil(1.*N/JOBMAX))):
     file.write('#!/bin/bash\n')
     file.write('#SBATCH --job-name ' + params['data_path']['parameterspace_label'] + '\n')    # set the name of the job
     file.write('#SBATCH --array 0-%d\n' % (batch_size-1))                                     # launch an array of jobs
-    file.write('#SBATCH --time 01:00:00\n')                                                   # specify a time limit
+    file.write('#SBATCH --time 24:00:00\n')                                                   # specify a time limit
     file.write('#SBATCH --ntasks 1\n')
     file.write('#SBATCH --cpus-per-task %d\n' % params['n_threads'])
     file.write('#SBATCH -o %s' % path + '/log/job_%A_%a.o\n')               # redirect stderr and stdout to the same file
     file.write('#SBATCH -e %s' % path + '/log/job_%A_%a.e\n')               # redirect stderr and stdout to the same file
     file.write('#SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE\n')              # send email notifications
     file.write('#SBATCH --mail-user=%s\n' % email)
+    file.write('#SBATCH --mem-per-cpu=10000\n')                                      # and reserve 6GB of memory
     file.write('source activate spiking-htm\n')                             # activate conda environment
-    file.write('#SBATCH --mem=6000\n')                                      # and reserve 6GB of memory
-    file.write('srun python %s %d $SLURM_ARRAY_TASK_ID %d \n' % (simulation_script,batch_id,JOBMAX) ) # call simulation script
+    file.write('srun python %s --exp-params %s --batch-id %d --exp-params-idx $SLURM_ARRAY_TASK_ID --jobmax %d\n' % (simulation_script, params_script, batch_id, JOBMAX) ) # call simulation script
     file.write('scontrol show jobid ${SLURM_JOBID} -dd # Job summary at exit')
     file.close()
     
