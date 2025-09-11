d71e655e4c2d9dc4a0639a43b19afc43bcad2aae
Younes Bouhadjar <ys.bouhadjar@gmail.com>
2025-09-11 17:44:22 +0200

    removed unnecessary files

--- auto_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-conda activate spiking-htm
-mkdir -p $PWD/build
-cd $PWD/build
- 
-cmake -DCMAKE_INSTALL_PREFIX=$PWD/ -Dwith-gsl=$CONDA_PREFIX -DREADLINE_ROOT_DIR=$CONDA_PREFIX -DLTDL_ROOT_DIR=$CONDA_PREFIX -Dwith-openmp=ON -Dwith-mpi=OFF ../../ && make -j8 && make install
