eed1f6d08a0d33384f98f12bdeb73c6e63f19991
Younes Bouhadjar <ys.bouhadjar@gmail.com>
2025-09-12 11:32:02 +0200

    Refactor unittest and rename filename

--- PyNEST/tests/test_build_network.py
+++ PyNEST/tests/test_build_network.py
@@ -1,21 +1,7 @@
-#!/bin/bash
-#
-# This file is part of spikingtemporalmemory.
-#
-# spikingtemporalmemory is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# spikingtemporalmemory is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with spikingtemporalmemory.  If not, see <https://www.gnu.org/licenses/>.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+#!/usr/bin/env python3
+"""
+Simple test script focused on model functionality from training.py
+"""
 
 import unittest
 import time
